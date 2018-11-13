5aa5889dcd704a2dc6962dbe26b4ba28ca9f6eac
Jake-parkers <joshuakwaku@outlook.com>
2018-11-13 03:40:49 +0100

    Added Endpoint urls and allowed public key to be passed via Rave Struct

--- rave/rave.go
+++ rave/rave.go
@@ -1,52 +1,163 @@
 package rave
 
 import (
-	"os"
+	"bytes"
+	"crypto/des"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
 	"log"
+	"os"
+	"strings"
+)
+
+const (
+	sandboxUrl string = "https://ravesandboxapi.flutterwave.com/"
+	liveUrl    string = "https://api.ravepay.co/"
 )
 
 // Rave base type
 type Rave struct {
-	Live bool
-	liveUrl string
-	testUrl string
+	Live      bool
 	PublicKey string
 	SecretKey string
 }
 
-// gets the correct url for live and test mode
-func (r Rave) getBaseURL() string {
-	if r.Live {
-		return r.liveUrl
-	}
-
-	return r.testUrl
+var Endpoints = map[string]map[string]string{
+	"card": {
+		"charge":          "flwv3-pug/getpaidx/api/charge",
+		"validate":        "flwv3-pug/getpaidx/api/validatecharge",
+		"verify":          "flwv3-pug/getpaidx/api/v2/verify",
+		"chargeSavedCard": "flwv3-pug/getpaidx/api/tokenized/charge",
+	},
+	"preauth": {
+		"charge":       "flwv3-pug/getpaidx/api/tokenized/preauth_charge",
+		"capture":      "flwv3-pug/getpaidx/api/capture",
+		"refundorvoid": "flwv3-pug/getpaidx/api/refundorvoid",
+	},
+	"account": {
+		"charge":   "flwv3-pug/getpaidx/api/charge",
+		"validate": "flwv3-pug/getpaidx/api/validate",
+		"verify":   "flwv3-pug/getpaidx/api/v2/verify",
+	},
+	"payment_plan": {
+		"create": "v2/gpx/paymentplans/create",
+		"fetch":  "v2/gpx/paymentplans/query",
+		"list":   "v2/gpx/paymentplans/query",
+		"cancel": "v2/gpx/paymentplans/",
+		"edit":   "v2/gpx/paymentplans/",
+	},
+	"subscriptions": {
+		"fetch":    "v2/gpx/subscriptions/query",
+		"list":     "v2/gpx/subscriptions/query",
+		"cancel":   "v2/gpx/subscriptions/",
+		"activate": "v2/gpx/subscriptions/",
+	},
+	"subaccount": {
+		"create": "v2/gpx/subaccounts/create",
+		"list":   "v2/gpx/subaccounts/",
+		"fetch":  "v2/gpx/subaccounts/get",
+	},
+	"transfer": {
+		"initiate":            "v2/gpx/transfers/create",
+		"bulk":                "v2/gpx/transfers/create_bulk",
+		"fetch":               "v2/gpx/transfers",
+		"fee":                 "v2/gpx/transfers/fee",
+		"balance":             "v2/gpx/balance",
+		"accountVerification": "flwv3-pug/getpaidx/api/resolve_account",
+	},
+	"verify": {
+		"verify": "flwv3-pug/getpaidx/api/v2/verify",
+	},
+	"refund": {
+		"refund": "gpx/merchant/transactions/refund",
+	},
 }
 
-// gets the public key
-func (r Rave) getPublicKey() string {
+// gets the correct url for live and test mode
+func (r Rave) GetBaseURL() string {
+	if r.Live {
+		return liveUrl
+	}
+
+	return sandboxUrl
+}
+
+func (Rave) GetEndpoint(endpointName string) map[string]string {
+	return Endpoints[endpointName]
+}
+
+// gets the public key from the environment variable if set or from the Rave object
+func (r Rave) GetPublicKey() string {
 	pubKey, ok := os.LookupEnv("RAVE_PUBKEY")
 	if !ok {
-		log.Fatal("You need to set the \"RAVE_PUBKEY\" environment variable")
+		if r.PublicKey == "" {
+			log.Fatal("You need to set the your public key as an environment variable \"RAVE_PUBKEY\" or as a field in thr Rave struct")
+		} else {
+			return r.PublicKey
+		}
 	}
 	return pubKey
 }
 
 // gets the secret key
-func (r Rave) getSecretKey() string {
+func (r Rave) GetSecretKey() string {
 	secKey, ok := os.LookupEnv("RAVE_SECKEY")
 	if !ok {
-		log.Fatal("You need to set the \"RAVE_SECKEY\" environment variable")
+		if r.SecretKey == "" {
+			log.Fatal("You need to set the your public key as an environment variable \"RAVE_SECKEY\" or as a field in thr Rave struct")
+		} else {
+			return r.SecretKey
+		}
 	}
 	return secKey
 }
 
-// constructor for Rave staruct
-func NewRave() Rave {
-	Rave := Rave{}
-	Rave.testUrl = "https://ravesandboxapi.flutterwave.com"
-	Rave.liveUrl = "https://api.ravepay.co"
-	Rave.Live = false
+func (r Rave) getKey(seckey string) string {
+	keymd5 := md5.Sum([]byte(seckey))
+	keymd5Last12 := keymd5[len(keymd5)-6:] // -6 because it's a hex byte array not a string
+	seckeyAdjusted := strings.Replace(seckey, "FLWSECK-", "", 1)
+	seckeyAdjustedFirst12 := seckeyAdjusted[:12]
 
-	return Rave
+	return seckeyAdjustedFirst12 + hex.EncodeToString(keymd5Last12[:])
+}
+
+func (r Rave) Encrypt(payload string) string {
+	seckey := r.GetSecretKey()
+	encryptedSecKey := r.getKey(seckey)
+
+	return r.encrypt3Des(encryptedSecKey, payload)
+
+}
+
+// Encrypt3Des : Encrypts the data using 3Des encryption
+func (r Rave) encrypt3Des(key string, payload string) string {
+	block, err := des.NewTripleDESCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+
+	bs := block.BlockSize() // block size is 8 by default
+	payloadBytes := pkcs5Padding([]byte(payload), bs)
+
+	if len(payloadBytes)%bs != 0 {
+		panic("Need a multiple of the blocksize")
+	}
+	encrypted := make([]byte, len(payloadBytes))
+	dst := encrypted
+
+	for len(payloadBytes) > 0 {
+		block.Encrypt(dst, payloadBytes[:bs])
+		payloadBytes = payloadBytes[bs:]
+		dst = dst[bs:]
+	}
+
+	return base64.StdEncoding.EncodeToString(encrypted)
+}
+
+// pkcs5Padding : Implements PKCS5 padding
+func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
 }
