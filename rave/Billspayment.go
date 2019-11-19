239b7708d62c1eefb5f0f61b719d0b4f4fbab4f9
unknown <olufemiobafunmiso>
2019-11-19 15:05:21 +0100

    Add Bills Payment

--- rave/Billspayment.go
+++ rave/Billspayment.go
@@ -1,22 +1,13 @@
 package rave
 
-// import (
-// 	"time"
-// )
+
 
 type Flybuy interface {
 	Flybuy(data FlyBuyData) (error error, response map[string]interface{})
 }
 
-// type FlyBuyBulk interface {
-// 	FlyBuyBulk(data FlyBuyBulkData) (error error, response map[string]interface{})
-// }
 
-// type FlyHistory interface {
-// 	Get(data FlyHistoryData) (error error, response map[string]interface{})
-// }
-
-type servicepayloaddata struct {
+type RequestsData struct {
 	Country       string `json:"Country"`
 	Amount        int    `json:"Amount"`
 	CustomerId    string `json:"CustomerId"`
@@ -25,31 +16,35 @@ type servicepayloaddata struct {
 	BillerName    string `json:"BillerName"`
 	Reference     string `json:"Reference"`
 }
-
-type FlyBuyData struct {
-	Service        string `json:"service"`
-	ServiceMethod  string `json:"service_method"`
-	ServiceVersion string `json:"service_version"`
-	ServiceChannel string `json:"service_channel`
-	ServicePayload servicepayloaddata
-	Seckey         string `json:"secret_key"`
+type ServicepayLoadData struct {
+	Country       string         `json:"Country"`
+	Amount        int            `json:"Amount"`
+	CustomerId    string         `json:"CustomerId"`
+	RecurringType int            `json:"RecurringType"`
+	IsAirtime     bool           `json:"IsAirtime"`
+	BillerName    string         `json:"BillerName"`
+	Reference     string         `json:"Reference"`
+	BatchRef      string         `json:"BatchReference"`
+	CallBackURL   string         `json:"CallBackUrl"`
+	Requests      []RequestsData `json:"Requests"`
 }
 
-// type FlyBuyBulkData struct {
-// 	Page   string `json:"page"`
-// 	Seckey string `json:"seckey"`
-// }
+type FlyBuyData struct {
+	Service        string             `json:"service"`
+	ServiceMethod  string             `json:"service_method"`
+	ServiceVersion string             `json:"service_version"`
+	ServiceChannel string             `json:"service_channel`
+	ServicePayload ServicepayLoadData `json:"service_payload"`
+	Seckey         string             `json:"secret_key"`
+}
 
-// type FlyHistoryData struct {
-// 	Id     int32  `json:"page"`
-// 	Seckey string `json:"seckey"`
-// }
+
 
 type Billpayment struct {
 	Rave
 }
 
-func (b Billpayment) Flybuy(data FlyBuyData) (error error, response map[string]interface{}) {
+func (b Billpayment) Bill(data FlyBuyData) (error error, response map[string]interface{}) {
 	data.Seckey = b.GetSecretKey()
 	url := b.GetBaseURL() + b.GetEndpoint("Billspayments", "flybuy")
 	err, response := MakePostRequest(data, url)
@@ -59,65 +54,3 @@ func (b Billpayment) Flybuy(data FlyBuyData) (error error, response map[string]i
 	return nil, response
 }
 
-// func (v Virtualcards) List(data ListData) (error error, response map[string]interface{}) {
-// 	data.Seckey = v.GetSecretKey()
-// 	url := v.GetBaseURL() + v.GetEndpoint("virtualcard", "list")
-// 	err, response := MakePostRequest(data, url)
-// 	if err != nil {
-// 		return err, noresponse
-// 	}
-// 	return nil, response
-// }
-
-// func (v Virtualcards) Get(data GetData) (error error, response map[string]interface{}) {
-// 	data.Seckey = v.GetSecretKey()
-// 	url := v.GetBaseURL() + v.GetEndpoint("virtualcard", "get")
-// 	err, response := MakePostRequest(data, url)
-// 	if err != nil {
-// 		return err, noresponse
-// 	}
-// 	return nil, response
-// }
-
-// func (v Virtualcards) Fund(data FundData) (error error, response map[string]interface{}) {
-// 	data.Seckey = v.GetSecretKey()
-// 	url := v.GetBaseURL() + v.GetEndpoint("virtualcard", "fund")
-// 	err, response := MakePostRequest(data, url)
-// 	if err != nil {
-// 		return err, noresponse
-// 	}
-// 	return nil, response
-// }
-
-// func (v Virtualcards) Withdraw(data WithdrawData) (error error, response map[string]interface{}) {
-// 	data.Seckey = v.GetSecretKey()
-// 	url := v.GetBaseURL() + v.GetEndpoint("virtualcard", "withdraw")
-// 	err, response := MakePostRequest(data, url)
-// 	if err != nil {
-// 		return err, noresponse
-// 	}
-// 	return nil, response
-// }
-
-// func (v Virtualcards) Freeze(data FreezeData) (error error, response map[string]interface{}) {
-// 	data.Seckey = v.GetSecretKey()
-// 	url := v.GetBaseURL() + v.GetEndpoint("virtualcard", "freeze")
-// 	url += data.CardId
-// 	url += "/status/"
-// 	url += data.StatusAction
-// 	err, response := MakePostRequest(data, url)
-// 	if err != nil {
-// 		return err, noresponse
-// 	}
-// 	return nil, response
-// }
-
-// func (v Virtualcards) Fetch(data FetchData) (error error, response map[string]interface{}) {
-// 	data.Seckey = v.GetSecretKey()
-// 	url := v.GetBaseURL() + v.GetEndpoint("virtualcard", "fetch")
-// 	err, response := MakePostRequest(data, url)
-// 	if err != nil {
-// 		return err, noresponse
-// 	}
-// 	return nil, response
-// }
